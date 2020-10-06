2513fcc4fc986cf8f2d20f0473a097822452c68c
sukesh2000 <sukesh.seth2000@gmail.com>
2020-10-07 00:09:22 +0530

    update

--- AWS-hadoop.py
+++ AWS-hadoop.py
@@ -5,87 +5,100 @@
 import os
 cmd = os.system
 
-def remove_spaces(file):
-    with open(file, 'r') as f:
-        lines = f.readlines()
-        f.close()
+class File(object):
+  files = []
+
+  def __init__(self,name):
+    self.name = name
+    File.files.append(self)
+  
+  def remove_spaces(self):
+    with open(self.name, 'r') as f:
+      lines = f.readlines()
+      f.close()
     lines = [line.strip() for line in lines]
     lines = [line + '\n' for line in lines]
-    with open(file, 'w') as f:
-        f.writelines(lines)
-        f.close()
+    with open(self.name, 'w') as f:
+      f.writelines(lines)
+      f.close()
 
 def main():
-    core = 'core-site.xml'
-    hdfs = 'hdfs-site.xml'
-    g = True
-    while g:
-        kind = input("For master enter 'M' & for slave enter 'S': ")
-        k = True
-        while k:
-            IP = (input('Enter the IP address: '))
-            ip = IP.split('.')
-            for bit in ip:
-                try:
-                    int(bit)
-                    k = False
-                except ValueError:
-                    print('Enter integer')
-        if kind.lower() == 'm':
-            name = 'name'
-            cmd('mkdir /nn')
-            directory = '/nn'
-            g = False
-        elif kind.lower() == 's':
-            name = 'data'
-            cmd('mkdir /dn')
-            directory = '/dn'
-            g = False
-        else:
-            print('Invalid input')
+  g = True
+  k = True
+  while g:
+    kind = input("For master enter 'M', for slave enter 'S' and for client enter 'C': ")
+    if kind.lower() == 'm':
+      name = 'name'
+      cmd('mkdir /nn')
+      directory = '/nn'
+      g = False
+    elif kind.lower() == 's':
+      name = 'data'
+      cmd('mkdir /dn')
+      directory = '/dn'
+      g = False
+    elif kind.lower() == 'c':
+      g = False
+    else:
+      print('Invalid input')
 
-    f1 = open(core,'w')
-    f1.write('''<?xml version="1.0"?>
-    <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+  while k:
+    IP = (input('Enter the IP address: '))
+    ip = IP.split('.')
+    for bit in ip:
+      try:
+        int(bit)
+        k = False
+      except ValueError:
+        print('Enter integer')
 
-    <!-- Put site-specific property overrides in this file. -->
+  core = File('core-site.xml')
+  f1 = open(core.name,'w')
+  f1.write('''<?xml version="1.0"?>
+  <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 
-    <configuration>
+  <!-- Put site-specific property overrides in this file. -->
 
-    <property>
-    <name>fs.default.name</name>
-    <value>hdfs://{}:9001</value>
-    </property>
+  <configuration>
 
-    </configuration>'''.format(IP))
+  <property>
+  <name>fs.default.name</name>
+  <value>hdfs://{}:9001</value>
+  </property>
 
-    f2 = open(hdfs,'w')
+  </configuration>'''.format(IP))
+  f1.close()
+
+  if (kind.lower() == 'm') or (kind.lower() == 's'):
+    print(kind)
+    hdfs = File('hdfs-site.xml')
+    f2 = open(hdfs.name,'w')
     f2.write('''<?xml version="1.0"?>
     <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 
     <!-- Put site-specific property overrides in this file. -->
 
     <configuration>
-            
+                
     <property>
     <name>dfs.{}.dir</name>
     <value>{}</value>
     </property>
 
     </configuration>'''.format(name,directory))
-            
-    f1.close()
     f2.close()
-    for file in (core,hdfs):
-        remove_spaces(file)
-    cmd('cp core-site.xml /etc/hadoop')
+            
+  for file in File.files:
+    file.remove_spaces()
+      
+  cmd('cp core-site.xml /etc/hadoop')
+  if (kind.lower() == 'm') or (kind.lower() == 's'):
     cmd('cp hdfs-site.xml /etc/hadoop')
 
-    if kind.lower() == 'm':
-        cmd('hadoop namenode -format')
-        cmd('hadoop-daemon.sh start namenode')
-    elif kind.lower() == 's':
-        cmd('hadoop-daemon.sh start datanode')
+  if kind.lower() == 'm':
+    cmd('yes | hadoop namenode -format')
+    cmd('hadoop-daemon.sh start namenode')
+  elif kind.lower() == 's':
+    cmd('hadoop-daemon.sh start datanode')
 if __name__ == '__main__':
-    main()
-
+  main()
--- AWS-hadoopsetup.sh
+++ AWS-hadoopsetup.sh
@@ -5,5 +5,4 @@ gdown --id 1541gbFeGZZJ5k9Qx65D04lpeNBw87rM5
 gdown --id 17UWQNVdBdGlyualwWX4Cc96KyZhD-lxz
 rpm -ivh jdk-8u171-linux-x64.rpm
 rpm -ivh hadoop-1.2.1-1.x86_64.rpm --force
-echo 3 > /proc/sys/vm/drop_caches
 python3 AWSHadoopSetup-CLI_Tool/AWS-hadoop.py
