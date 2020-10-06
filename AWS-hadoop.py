3c1d4c9dfd62fa5f1b90534ba91ef2403cfacb8a
sukesh2000 <sukesh.seth2000@gmail.com>
2020-10-07 01:22:29 +0530

    update

--- AWS-hadoop.py
+++ AWS-hadoop.py
@@ -95,7 +95,7 @@ def main():
     cmd('cp hdfs-site.xml /etc/hadoop')
 
   if kind.lower() == 'm':
-    cmd('yes | hadoop namenode -format')
+    cmd('Y | hadoop namenode -format')
     cmd('hadoop-daemon.sh start namenode')
   elif kind.lower() == 's':
     cmd('hadoop-daemon.sh start datanode')
